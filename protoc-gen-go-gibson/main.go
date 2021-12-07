c6c1c3ac19f49cab520b03b533879e7825e6684a
Marcelo Rodriguez <marcelo@simplefxn.com>
2021-12-06 18:22:18 -0600

    Simple template for server

--- protoc-gen-go-gibson/main.go
+++ protoc-gen-go-gibson/main.go
@@ -23,24 +23,66 @@ import (
 var templateDir embed.FS
 
 type method struct {
-	Name          string
-	NameLowerCase string
-	Request       string
-	Response      string
+	Name           string
+	NameLowerCase  string
+	Request        string
+	Response       string
+	IsStreamServer bool
+	IsStreamClient bool
 }
 
 // service contains all the necessary information to generate the files
-type serivce struct {
-	ImportPath           string
-	Package              string
-	ServiceName          string
-	ServiceNameLowerCase string
-	Methods              []method
+type service struct {
+	Name          string
+	NameLowerCase string
+	Methods       []method
+}
+
+type protoDefinition struct {
+	Package    string
+	ImportPath string
+	GoPackage  string
+	services   []service
+}
+
+func (p protoDefinition) dump() {
+	log.Printf("Package: %s", p.Package)
+	log.Printf("ImportPath: %s", p.ImportPath)
+	log.Printf("GoPackage: %s", p.GoPackage)
+
+	for _, s := range p.services {
+		log.Printf("Name: %s", s.Name)
+		log.Printf("NameLowerCase: %s", s.NameLowerCase)
+		for _, m := range s.Methods {
+			log.Printf("Name: %s", m.Name)
+			log.Printf("NameLowerCase: %s", m.NameLowerCase)
+			log.Printf("Request: %s", m.Request)
+			log.Printf("Response: %s", m.Response)
+			log.Printf("IsStreamServer: %t", m.IsStreamServer)
+			log.Printf("IsStreamClient: %t", m.IsStreamClient)
+			log.Printf("-------------------------------------------------------")
+		}
+	}
 }
 
 var generatedFiles = map[string]string{
-	"./cmd/{svc}/main.go":    "templates/cmd/main.tmpl",
-	"./pkg/server/server.go": "templates/server/server.tmpl",
+	"./Makefile":                "templates/Makefile.tmpl",
+	"./go.mod":                  "templates/go.mod.tmpl",
+	"./main.go":                 "templates/main.go.tmpl",
+	"./cmd/client.go":           "templates/cmd/client.go.tmpl",
+	"./cmd/root.go":             "templates/cmd/root.go.tmpl",
+	"./cmd/run.go":              "templates/cmd/run.go.tmpl",
+	"./cmd/server.go":           "templates/cmd/server.go.tmpl",
+	"./common/logger/logger.go": "templates/common/logger/logger.go.tmpl",
+	"./common/logger/zap.go":    "templates/common/logger/zap.go.tmpl",
+	"./common/common.go":        "templates/common/common.go.tmpl",
+	"./service/service.go":      "templates/service/service.go.tmpl",
+	"./service/grpc/crl.go":     "templates/service/grpc/crl.go.tmpl",
+	"./service/grpc/server.go":  "templates/service/grpc/server.go.tmpl",
+	"./service/grpc/tls.go":     "templates/service/grpc/tls.go.tmpl",
+	"./service/http/server.go":  "templates/service/http/server.go.tmpl",
+	//"./cmd/{svc}/main.go":    "templates/cmd/main.tmpl",
+	//"./pkg/server/server.go": "templates/server/server.tmpl",
 }
 
 func firstLetterToLowerCase(s string) string {
@@ -50,36 +92,40 @@ func firstLetterToLowerCase(s string) string {
 }
 
 func main() {
-	svc, err := parseProto()
+	//_p, err := parseProto()
+	_p, err := parseProto()
 	if err != nil {
 		panic(fmt.Errorf("failed to parse proto file %w", err))
 	}
 
+	//_p.dump()
 	for f, t := range generatedFiles {
-		if err := createFileFromTemplate(svc, f, t); err != nil {
+		if err := createFileFromTemplate(_p.Package, _p.services[0], f, t); err != nil {
 			panic(fmt.Errorf("failed to create file %v: %w", f, err))
 		}
 	}
 
 	// create the folder for go_out and grpc_out
-	if err := os.Mkdir("./pkg/pb", 0775); err != nil {
+	if err := os.MkdirAll("./pkg/pb", 0775); err != nil {
 		if errors.Is(err, os.ErrExist) {
 			panic(err)
 		}
 	}
+
 }
 
-func parseProto() (svc serivce, err error) {
+func parseProto() (protoDefinition, error) {
+	var err error
+	p := protoDefinition{}
+
 	input, err := io.ReadAll(os.Stdin)
 	if err != nil {
-		return svc, fmt.Errorf("io.ReadAll(): %w", err)
+		return p, fmt.Errorf("io.ReadAll(): %w", err)
 	}
-
-	log.Println(string(input))
 
 	req := pluginpb.CodeGeneratorRequest{}
 	if err = proto.Unmarshal(input, &req); err != nil {
-		return svc, fmt.Errorf("proto.Unmarshal(): %w", err)
+		return p, fmt.Errorf("proto.Unmarshal(): %w", err)
 	}
 
 	opts := protogen.Options{}
@@ -87,39 +133,35 @@ func parseProto() (svc serivce, err error) {
 	plugin, err := opts.New(&req)
 
 	if err != nil {
-		return svc, fmt.Errorf("protogen.Options.New(): %w", err)
+		return p, fmt.Errorf("protogen.Options.New(): %w", err)
 	}
 
 	// Files appear in topological order, so each file appears before any
 	// file that imports it.
 	f := plugin.Files[len(plugin.Files)-1]
 
-	log.Println(f.GoPackageName)
-	log.Println(f.GeneratedFilenamePrefix)
-	log.Println(f.GoDescriptorIdent.String())
-	log.Println(f.Desc.FullName().Name())
-	log.Println(f.Desc.ParentFile().Path())
-	log.Println(f.Desc.Path())
-	log.Println(f.GoImportPath.String())
-	log.Println(f.Desc.Name())
+	p.Package = string(*f.Proto.Package)
+	p.GoPackage = string(f.GoPackageName)
+	p.ImportPath = filepath.Dir(string(f.GoImportPath))
 
-	svc.Package = string(f.GoPackageName)
-	svc.ImportPath = filepath.Dir(string(f.GoImportPath))
-	svc.ServiceName = strings.Title(f.Services[0].GoName)
-	svc.ServiceNameLowerCase = firstLetterToLowerCase(svc.ServiceName)
+	for _, s := range f.Services {
+		svc := service{}
+		svc.Name = strings.Title(s.GoName)
+		svc.NameLowerCase = firstLetterToLowerCase(svc.Name)
 
-	for _, m := range f.Services[0].Methods {
-		var svcMethod method
+		for _, m := range f.Services[0].Methods {
+			var svcMethod method
+			svcMethod, err = parseMethod(m)
 
-		svcMethod, err = parseMethod(m)
-		if err != nil {
-			return
+			if err != nil {
+				return p, err
+			}
+			svc.Methods = append(svc.Methods, svcMethod)
 		}
-
-		svc.Methods = append(svc.Methods, svcMethod)
+		p.services = append(p.services, svc)
 	}
 
-	return
+	return p, err
 }
 
 func parseMethod(m *protogen.Method) (svcMethod method, err error) {
@@ -128,12 +170,14 @@ func parseMethod(m *protogen.Method) (svcMethod method, err error) {
 	svcMethod.NameLowerCase = firstLetterToLowerCase(svcMethod.Name)
 	svcMethod.Request = m.Input.GoIdent.GoName
 	svcMethod.Response = m.Output.GoIdent.GoName
+	svcMethod.IsStreamClient = m.Desc.IsStreamingClient()
+	svcMethod.IsStreamServer = m.Desc.IsStreamingServer()
 
 	return
 }
 
-func createFileFromTemplate(svc serivce, filePath, templatePath string) (err error) {
-	filePath = strings.Replace(filePath, "{svc}", svc.ServiceNameLowerCase, -1)
+func createFileFromTemplate(packageName string, svc service, filePath, templatePath string) (err error) {
+	filePath = strings.Replace(filePath, "{svc}", svc.NameLowerCase, -1)
 
 	if err = os.MkdirAll(filepath.Dir(filePath), 0775); err != nil {
 		return fmt.Errorf("os.MkdirAll() %w", err)
@@ -150,13 +194,22 @@ func createFileFromTemplate(svc serivce, filePath, templatePath string) (err err
 		return fmt.Errorf("unable to read template file: %w", err)
 	}
 
-	t := template.New(templatePath)
+	funcMap := template.FuncMap{
+		"ToLower": strings.ToLower,
+	}
+
+	t := template.New(templatePath).Funcs(funcMap)
 	t, err = t.Parse(string(b))
 	if err != nil {
 		return fmt.Errorf("unable to parse template file: %w", err)
 	}
 
-	if err = t.Execute(f, svc); err != nil {
+	data := struct {
+		PackageName string
+		Service     service
+	}{PackageName: packageName, Service: svc}
+
+	if err = t.Execute(f, data); err != nil {
 		return fmt.Errorf("error executing template %v:%w", t.Name(), err)
 	}
 
